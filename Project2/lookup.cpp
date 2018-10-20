44d42c53c5eff53d3ca3d31ccc269c2edfd3b9ae
spencer-danielson <danielsons1@udayton.edu>
2018-10-20 17:55:22 -0400

    not working
    getting weird received bytes.. URL is not correct

--- Project2/lookup.cpp
+++ Project2/lookup.cpp
@@ -1,36 +1,37 @@
 #include "headers.h"
 #include "lookup.h"
+#include <iostream>
 
 Question::Question(void)  // constructor
 {
 
 };
-<<<<<<< HEAD
 
 // Packet visual:
 // [ DNS Header - Question Buffer - Query Header ]
-bool Question::MakePacket(u_char* pkt, FixedDNSheader &dnsheader, QueryHeader &queryheader)
+bool Question::MakePacket(char* pkt, FixedDNSheader &dnsheader, QueryHeader &queryheader)
 {
-	int hdr_size = sizeof(FixedDNSheader);
+	int size_pkt = strlen(pkt);
+	int dhdr_size = sizeof(dnsheader);
+	int qhdr_size = sizeof(queryheader);
 	int buf_size = sizeof(rawbuffer);
 
 	// add the dns header to pkt
-	memcpy(pkt, &dnsheader, hdr_size);
+	memcpy(pkt, &dnsheader, dhdr_size);
 
 	// copy the mem from rawbuffer to pkt
-	memcpy(pkt + hdr_size, rawbuffer, sizeof(rawbuffer));
+	memcpy(pkt + dhdr_size, rawbuffer, buf_size);
 
 	// add the query header to pkt
-	memcpy(pkt + hdr_size + buf_size, &queryheader, hdr_size);
+	//memcpy(pkt + size_pkt - qhdr_size, &queryheader, qhdr_size);
 
 	return true;
-
 }
 
 bool Question::CreateQuestion(string host)
 {
-	rawbuffer = new u_char[
-		sizeof(FixedDNSheader) + sizeof(QueryHeader) + host.size() + 2];
+	// only creating the question, so only use size of question
+	rawbuffer = new char[host.size() + 2];
 
 	int position = host.find(".");
 	string sub_str;
@@ -44,55 +45,24 @@ bool Question::CreateQuestion(string host)
 		sub_size = position - i;
 		sub_str = host.substr(i, position);
 
-		rawbuffer[hdr_size + i] = sub_size;  // specify the size of the chunk (subdomain)
+		rawbuffer[i] = sub_size;  // specify the size of the chunk (subdomain)
 		i++;
-		memcpy(rawbuffer + hdr_size + i, sub_str.c_str(), sub_size); // specify the actual subdomain
+		memcpy(rawbuffer + i, sub_str.c_str(), sub_size); // specify the actual subdomain
 
 		i += sub_size;
 		position = host.find(".", i);
 	}
 	rawbuffer[hdr_size + i] = 0;
 
-=======
-
-bool Question::MakePacket(u_char* pkt, FixedDNSheader &dnsheader, QueryHeader &queryheader)
-{
+	for (int i = 0; i < host.size() + 2; i++)
+	{
+		std::cout << "i= " << i << " " << rawbuffer[i] << endl;
+	}
+	getchar();
 
 	return true;
-
 }
 
-bool Question::CreateQuestion(string host) {
-
-	//+1 byte for "size" for last substring, +1 for "0" meaning the end of question
-	//int pkt_size = sizeof(FixedDNSheader) + sizeof(QueryHeader) + host.size() + 2;
-
-	//char* pkt = new char[pkt_size];
-	int position = host.find(".");
-	string sub_str;
-
-	int i = 0, sub_size = 0, hdr_size = sizeof(FixedDNSheader);
-
-	// parse the host and place contents in packet
-	host += ".";
-	while (position != -1)
-	{
-		sub_size = position - i;
-		sub_str = host.substr(i, position);
-
-		rawbuffer[hdr_size + i] = sub_size;  // specify the size of the chunk (subdomain)
-		i++;
-		memcpy(rawbuffer + hdr_size + i, sub_str.c_str(), sub_size); // specify the actual subdomain
-
-		i += sub_size;
-		position = host.find(".", i);
-	}
-	rawbuffer[hdr_size + i] = 0;
-
-	// add the query type and query class to the end of packet
-	//pkt[hdr_size + i + 1] = DNS_PTR;
-	//pkt[hdr_size + i + 2] = DNS_INET;
-
->>>>>>> a56550e6247b47a417093b77c150a1b220b1224c
-	return true;
+int Question::Size() {
+	return sizeof(rawbuffer);
 }
